6b0b1b85e4157edbb39ec0c7a562d05308682b96
ec-johnston <ecj@stanford.edu>
2021-06-02 21:59:02 -0700

    add files

---
+++ analysis_scripts/panel_regression_models.R
@@ -0,0 +1,15 @@
+library(lfe)
+
+## The following strings specify model formulas (modform) for bootstrap_models.R 
+
+## EQUATION 1 (base model): panel regression with grid cell and year fixed effects
+eq_1 <- landslides ~ precip:land_use | year + grid_cell
+
+## EQUATION 2: includes an interaction with mean precipitation
+eq_2 <- landslides ~ precip:land_use + precip:mean_precip:land_use | year + grid_cell
+
+## EQUATION 3: includes an interaction with mean slope
+eq_3 <- landslides ~ precip:land_use + precip:slope:land_use | year + grid_cell
+
+## EQUATION 4: includes interactions with mean precipitation and mean slope in the same equation 
+eq_4 <- landslides ~ precip:land_use + precip:mean_precip:land_use + precip:slope:land_use | year + grid_cell
