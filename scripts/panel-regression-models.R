c410a643b6559731894be81c94fe298419cf876f
Elizabeth Johnston <38542249+ec-johnston@users.noreply.github.com>
2021-06-18 11:24:31 -0700

    Update panel-regression-models.R

--- scripts/panel-regression-models.R
+++ scripts/panel-regression-models.R
@@ -2,8 +2,12 @@ library(lfe)
 
 ## The following strings define panel regression model formulas (i.e., modform) used for bootstrap_models.R  
 
-## EQUATION 1 (base model): panel regression with grid cell and year fixed effects
+## MAIN MODEL 
+
+## EQUATION 1: panel regression with grid cell and year fixed effects
 eq_1 <- landslides ~ precip:land_use | year + grid_cell
+
+## SUPPLEMENTAL FORMS
 
 ## EQUATION 2: includes an interaction with mean precipitation
 eq_2 <- landslides ~ precip:land_use + precip:mean_precip:land_use | year + grid_cell
