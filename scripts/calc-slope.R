b06e29c5c1ab06ed63a161764e10a7f931451fe3
Elizabeth Johnston <38542249+ec-johnston@users.noreply.github.com>
2021-06-18 15:56:43 -0700

    Update calc-slope.R

--- scripts/calc-slope.R
+++ scripts/calc-slope.R
@@ -1,6 +1,6 @@
 library(raster)
 
-source("~/landslides-precip/func.R")
+source("~/ec-johnston/landslides-precip/func.R")
 
 
 ## Digital Elevation Model (with SRTM voids filled using accurate topographic mapping) 
