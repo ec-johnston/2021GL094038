8814e5aac00dd9f62a8ee3c1a94ea803e0de3b7c
Elizabeth Johnston <38542249+ec-johnston@users.noreply.github.com>
2021-06-18 11:32:49 -0700

    Update array-sbatch.sh

--- scripts/array-sbatch.sh
+++ scripts/array-sbatch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=boot_all
-#SBATCH --error=/oak/stanford/groups/*******/group_members/***/landslide_felm_PRISM/felm_01d/pacificCoast_states/main/felm_boot_%a.err
-#SBATCH --output=/oak/stanford/groups/*******/group_members/***/landslide_felm_PRISM/felm_01d/pacificCoast_states/main/felm_boot_%a.out
+#SBATCH --error=/oak/stanford/groups/xxxxxxx/group_members/xxx/landslide_felm_PRISM/felm_01d/pacificCoast_states/main/felm_boot_%a.err
+#SBATCH --output=/oak/stanford/groups/xxxxxxx/group_members/xxx/landslide_felm_PRISM/felm_01d/pacificCoast_states/main/felm_boot_%a.out
 #SBATCH --array=1-200%5
 #SBATCH --time=2:00:00
 #SBATCH --ntasks=1
@@ -17,6 +17,6 @@ ml load physics gdal
 ml load physics proj 
 ml load udunits
 
-cd /oak/stanford/groups/*******/group_members/***/landslide_felm_PRISM/felm_01d/pacificCoast_states/main/
+cd /oak/stanford/groups/xxxxxxx/group_members/xxx/landslide_felm_PRISM/felm_01d/pacificCoast_states/main/
 ## arguements for bootstrapping are: ARRAY_ID NBOOT modname modform datafile cluster_varname
 Rscript ./bootstrap-models.R $SLURM_ARRAY_TASK_ID 5 felm "landslides ~ precip:land_use | year + grid_cell" "./dataframe_01d.rds" ID 
