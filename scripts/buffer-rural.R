e24b2fe76b2e88724fdc6e7ca4cd6d21e5699784
Elizabeth Johnston <38542249+ec-johnston@users.noreply.github.com>
2021-07-16 09:49:18 -0700

    Update buffer-rural.R

--- scripts/buffer-rural.R
+++ scripts/buffer-rural.R
@@ -2,13 +2,13 @@ library(rgdal)
 library(raster)
 library(tidyverse)
 
-source("~/landslides-precip/scripts/func.R")
+source("./func.R")
 
 ## read in raster and dataframe of the study area (in this case, the Pacific Coast region of the coterminous US)
-pacific_coast <- raster("~/landslides-precip/data/pacific_coast.asc")
+pacific_coast <- raster("./pacific_coast.asc")
 crs(pacific_coast) <- "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0" 
-pacific_coast_df <- readRDS("~/landslides-precip/data/pacific_coast_df.rds")
-land_use <- readRDS("~/landslides-precip/data/land_use.rds")
+pacific_coast_df <- readRDS("./pacific_coast_df.rds")
+land_use <- readRDS("./land_use.rds")
 
 ## The TIGER/Line shapefile of land use designations is available from the US Census (http://data.census.gov)
 ## Read in the latest TIGER/Line shapefile for the US as a SpatialPolygonsDataFrame
